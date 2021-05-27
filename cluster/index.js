9243d03d4f0f94d3be603e1c70dda230dd6d257a
pphu <pphu@tencent.com>
2021-05-27 21:11:56 +0800

    feat: add

--- cluster/index.js
+++ cluster/index.js
@@ -1,7 +1,8 @@
 const cluster = require('cluster');
 const http = require('http');
-const numCPUs = require('os').cpus().length;
+const numCPUs = require('os').cpus().length / 2;
 
+console.log(`index exec`)
 if (cluster.isMaster) {
   console.log(`主进程 ${process.pid} 正在运行`);
 
@@ -11,12 +12,16 @@ if (cluster.isMaster) {
   }
 
   cluster.on('exit', (worker, code, signal) => {
+    console.log(`cluster exit`, worker, code, signal)
     console.log(`工作进程 ${worker.process.pid} 已退出`);
   });
 } else {
+  console.log(`enter else`);
   // 工作进程可以共享任何 TCP 连接。
   // 在本例子中，共享的是 HTTP 服务器。
   http.createServer((req, res) => {
+    console.log(`这是子进程 ${process.pid}, 我接收了请求处理并返回`)
+    res.setHeader('Content-Type', 'application/json; charset=utf-8') // 这里不设置编码会乱
     res.writeHead(200);
     res.end('你好世界\n');
   }).listen(8000);
--- readme.md
+++ readme.md
@@ -28,3 +28,14 @@ sysctl machdep.cpu | grep machdep.cpu.thread_count # mac超线程，一般为双
 ```
 
 # node cluster启动多进程
+## 按照cps核数启动多个工作进程worker
+
+```
+const numCPUs = require('os').cpus().length / 2; // 这里是因为mac cpu超频了，默认double逻辑核心数
+```
+
+## isMater
+一个属性，标识进程是主/子进程，执行逻辑大概是这样的：主进程isMater是true，fork了多个子进程，程序执行结束。
+子进程生成后继续执行，但是isMaster为false，进入下面逻辑，创建server并监听8000端口
+
+每个请求只会被分配到一个worker执行，哪个worker被分配是随机的
